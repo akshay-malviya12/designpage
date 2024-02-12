8078cb7eb57a123e8283af00aca8c9704b0282b0
akshay malviya <102719327+akshay-malviya12@users.noreply.github.com>
2024-02-12 10:42:03 +0530

    Update script.js
    Add comment and remove unused code.

--- script.js
+++ script.js
@@ -1,37 +1,5 @@
-// const boxCheckboxes = document.querySelectorAll('.box-checkbox');
 
-// for (let i = 0; i < boxCheckboxes.length; i++) {
-//   const checkbox = boxCheckboxes[i];
-//   const box = checkbox.closest('.box');
-//   const selectedInput = box.querySelector('.selected-input');
-
-//   box.addEventListener('mouseenter', () => {
-//     box.classList.add('highlighted')
-//     selectedInput.style.display = 'block';
-
-//   });
-
-//   box.addEventListener('mouseleave', () => {
-//     if (!checkbox.checked) {
-//       box.classList.remove('highlighted');
-//       selectedInput.style.display = 'none';
-//     }
-//   });
-
-//   checkbox.addEventListener('change', () => {
-//     if (checkbox.checked) {
-//       selectedInput.style.display = 'none';
-//     } else {
-//       selectedInput.style.display = 'block';
-//     }
-//   });
-// }
-
-
-
-
-
-
+//get box by id
 const boxes = document.querySelectorAll('.box');
 
 for (let i = 0; i < boxes.length; i++) {
@@ -39,12 +7,14 @@ for (let i = 0; i < boxes.length; i++) {
   const checkbox = box.querySelector('.box-checkbox');
   const selectedInput = box.querySelector('.selected-input');
 
+  //mouse enter box is highlighted and radio button checked.
   box.addEventListener('mouseenter', () => {
     box.classList.add('highlighted');
     selectedInput.style.display = 'block';
     box.querySelector('input[type="radio"]').checked = true;
   });
 
+  //mouse leave highlighted class remove and redio button off.
   box.addEventListener('mouseleave', () => {
     if (!checkbox.checked) {
       box.classList.remove('highlighted');
